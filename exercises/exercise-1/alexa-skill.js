62a6dede1ba510575a4be8b21624880ebb27166f
sparkle222 <annissa.alusi@gmail.com>
2017-03-21 14:49:25 -0700

    Update alexa-skill.js

--- exercises/exercise-1/alexa-skill.js
+++ exercises/exercise-1/alexa-skill.js
@@ -80,7 +80,7 @@ var main = function (event) {
                     });
                 alexaSDK.APP_ID = APP_ID;
                 alexaSDK.resources = languageStrings;
-
+                alexaSDK.registerHandlers(newSessionHandlers);
                 return alexaSDK.execute();
             } catch (err) {
                 console.log(err);
